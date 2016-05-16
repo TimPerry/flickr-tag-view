d8c8d1289d75d173d0a70bd744baf4d13b4348ff
Tim Perry <tim.r.perry@gmail.com>
2016-05-16 19:22:48 +0100

    Adding some basic styles

--- src/app.jsx
+++ src/app.jsx
@@ -7,9 +7,9 @@ var App = React.createClass({
     render: function() {
 	return (
 		<main>
-		<section>
+		<section class="imageGrid">
 		{this.props.images.map(function(image, index) {
-		    return <img key={index} src={image.imageURL}/>
+		    return <img key={index} src={image.imageURL} className="imageGrid__image"/>
 		})}
      	    </section>
 		</main>
